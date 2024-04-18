8ca4b369f3650c087a18c95ab9c42eb54ecf943b
ARGAO <jgabrielaragao@gmail.com>
2024-04-18 16:12:13 -0300

    tupla

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -4,15 +4,125 @@ using System.Reflection.Metadata;
 using ExemploExplorando.Models;
 
 
-Dictionary<string, string> estados = new Dictionary<string, string>();
-
-estados.Add("SP", "São Paulo");
-estados.Add("RJ", "Rio de Janeiro");
-estados.Add("MG", "Minas Gerais");
 
 
 
-Console.WriteLine(estados["SP"]);
+int numero = 10;
+
+//if ternário
+bool ehPar = numero % 2 == 0;
+Console.WriteLine($"O numero {numero} é {(ehPar ? "par" : "ímpar")}");
+
+// if(numero % 2 == 0)
+// {
+//     Console.WriteLine($"O número {numero} é par");
+// }
+// else
+// {
+//     Console.WriteLine($"O número {numero} é ímpar");
+// }
+
+
+
+
+
+// Pessoa p1 = new Pessoa(nome: "João",sobrenome: "Aragão");
+
+// ( string nome, string sobrenome) = p1;
+
+
+// Console.WriteLine($"{nome} {sobrenome}");
+
+
+
+
+
+
+
+
+
+
+
+// LeituraArquivo arquivo = new LeituraArquivo();
+
+// var (sucesso, linhasArquivo, _) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
+
+
+// if(sucesso)
+// {
+    
+//     foreach (var linha in linhasArquivo)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Erro ao ler o arquivo");
+// }
+
+
+
+
+
+
+// //Maneiras de criar uma tupla
+// (int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "João", "Aragão", 1.8m);
+// ValueTuple<int, string, string, decimal> tupla2 = (2, "Eduardo", "Neves", 1.7m);
+// var tupla3 = (3, "Maria", "Silva", 1.6m);
+
+// Console.WriteLine($"Id: {tupla.Id}");
+// Console.WriteLine($"Nome: {tupla.Nome}");
+// Console.WriteLine($"Sobrenome: {tupla.Sobrenome}");
+// Console.WriteLine($"Altura: {tupla.Altura}");
+
+
+// Console.WriteLine($"Id: {tupla2.Item1}");
+// Console.WriteLine($"Nome: {tupla2.Item2}");
+// Console.WriteLine($"Sobrenome: {tupla2.Item3}");
+// Console.WriteLine($"Altura: {tupla2.Item4}");
+
+// Console.WriteLine($"Id: {tupla3.Item1}");
+// Console.WriteLine($"Nome: {tupla3.Item2}");
+// Console.WriteLine($"Sobrenome: {tupla3.Item3}");
+// Console.WriteLine($"Altura: {tupla3.Item4}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Dictionary<string, string> estados = new Dictionary<string, string>();
+
+// estados.Add("SP", "São Paulo");
+// estados.Add("RJ", "Rio de Janeiro");
+// estados.Add("MG", "Minas Gerais");
+
+
+
+// Console.WriteLine(estados["SP"]);
 
 
 // foreach (var estado in estados)
@@ -128,15 +238,6 @@ Console.WriteLine(estados["SP"]);
 
 
 // new ExemploExcecao().Metodo1();
-
-
-
-
-
-
-
-
-
 
 
 
